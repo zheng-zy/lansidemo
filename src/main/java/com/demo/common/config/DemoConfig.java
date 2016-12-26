47ba63fbc3f58724b58bbd94977ac29e1ad9d181
zheng-zy <zhezhiyong@163.com>
2016-12-26 21:05:12 +0800

    easyui index

--- src/main/java/com/demo/common/config/DemoConfig.java
+++ src/main/java/com/demo/common/config/DemoConfig.java
@@ -46,7 +46,7 @@ public class DemoConfig extends JFinalConfig {
      * 配置路由
      */
     public void configRoute(Routes me) {
-        me.add("/", IndexController.class, "/index");    // 第三个参数为该Controller的视图存放路径
+        me.add("/", IndexController.class, "/");    // 第三个参数为该Controller的视图存放路径
         me.add("/blog", BlogController.class);            // 第三个参数省略时默认与第一个参数值相同，在此即为 "/blog"
         me.add("/user", UserController.class);            // 第三个参数省略时默认与第一个参数值相同，在此即为 "/blog"
     }
